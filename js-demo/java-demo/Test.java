b6a3a4c75d0ee0ce64f2741018e75ef3d00d0814
yksdy <yksdy_blue@icloud.com>
2019-08-25 09:23:45 +0800

    update

---
+++ js-demo/java-demo/Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args){
+        int a=1;
+        int b=a;
+        a=3;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
